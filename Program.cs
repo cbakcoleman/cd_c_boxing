ec0ab34ae60eca4c957748169f3851f567b07e68
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-27 20:25:33 -0700

    commit assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace cd_c_boxing
 {
@@ -6,7 +7,39 @@ namespace cd_c_boxing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create an mpty list of type object
+            List<object> list1 = new List<object>();
+
+            // add the following values to the list:
+            list1.Add(7);
+            list1.Add(28);
+            list1.Add(-1);
+            list1.Add(true);
+            list1.Add("chari");
+
+            // Loop through the list and print all values
+            for (var idx = 0; idx < list1.Count; idx++)
+            if (list1[idx] is int)
+            {
+                System.Console.WriteLine(list1[idx]);
+            }
+            else if (list1[idx] is string)
+            {
+                System.Console.WriteLine(list1[idx]);
+            }
+            else if (list1[idx] is bool)
+            {
+                System.Console.WriteLine(list1[idx]);
+            }
+
+            // Add all values that are int type together and output the sum
+            int sum = 0;
+            for (var idx = 0; idx < list1.Count; idx++)
+            if (list1[idx] is int)
+            {
+                sum = sum + list1[idx];
+            }
+            System.Console.WriteLine(sum);
         }
     }
 }
